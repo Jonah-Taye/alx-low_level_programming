b1fff0ea6a8fbc0803061726b24896e047583b74
Jonah <yonastaye859@gmail.com>
2022-09-08 21:13:03 +0000

    second update for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	char printText = "\"Programming is like building a multilingual puzzle\n";
+	char printText[] = "\"Programming is like building a multilingual puzzle\n";
 	
 	puts(printText);
 	return (0);
