274437d4d1a70a6ce95fe2bd03d3c732452219b8
Jonah <yonastaye859@gmail.com>
2022-09-08 21:15:41 +0000

    third update for task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char printText[] = "\"Programming is like building a multilingual puzzle\n";
-	
+	char printText[] = "\"Programming is like building a multilingual puzzle";
 	puts(printText);
 	return (0);
 }
