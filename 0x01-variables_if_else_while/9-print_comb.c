cd38e4c6ede754fa054e56d0624d001b109cfc96
Jonah <yonastaye859@gmail.com>
2022-09-09 21:25:32 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers 0 to 9 using putchar() function
+ * and adds a comma between the numbers
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers = 48;
+	int comma = 44;
+
+	while (numbers < 58)
+	{
+		putchar(numbers);
+		if (numbers < 57)
+		{
+			putchar(' ');
+			putchar(comma);
+			numbers++;
+		}
+		else
+			numbers++;
+	}
+	putchar('\n');
+
+	return (0);
+}
