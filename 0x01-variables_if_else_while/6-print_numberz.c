ba3be76dd5868b302054c8a7fbd7d7dbc9d6292b
Jonah <yonastaye859@gmail.com>
2022-09-09 20:45:08 +0000

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints the numbers 0 to 9 using putchar() function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers = 48;
+
+	while (numbers < 58)
+	{
+		putchar(numbers);
+		numbers++;
+	}
+        putchar('\n');
+
+	return (0);
+}
