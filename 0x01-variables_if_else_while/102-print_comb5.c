d7a3914e1c81529d2d2c959b51f1d58014b130f2
Jonah <yonastaye859@gmail.com>
2022-09-10 01:49:04 +0000

    second update for task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,40 +3,27 @@
  * main - Prints two two-digit numbers without the two numbers repeating
  *
  * Return: Always 0 (Success)
- */
+ **/
 int main(void)
 {
-	int digit1;
-	int digit2;
-	int digit3;
-	int digit4;
-	int comma = 44;
+	int i, j;
 
-	for (digit1 = 48; digit1 < 58; digit1++)
+	for (i = 0; i < 100; i++)
 	{
-		for (digit2 = 48; digit2 < 57; digit2++)
+		for (j = i + 1; j < 100; j++)
 		{
-			for (digit3 = digit1; digit3 < 58; digit3++)
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+			if (i != 98 || j != 99)
 			{
-				for (digit4 = digit2; digit4 < 58; digit4++)
-				{
-					if (digit1 == digit3 && digit2 == digit4)
-						digit4++;
-					putchar(digit1);
-					putchar(digit2);
-					putchar(' ');
-					putchar(digit3);
-					putchar(digit4);
-					if (digit2 != 56)
-					{
-						putchar(comma);
-						putchar(' ');
-					}
-				}
+				putchar(',');
+				putchar(' ');
 			}
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
