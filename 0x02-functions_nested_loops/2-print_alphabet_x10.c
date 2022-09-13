364527be26586ebbb3e08c7d3624f2e556203397
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 18:27:09 +0300

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabets in lowercase 10 times
+ *
+ * Return: Void
+ */
+void print_alphabet_x10(void)
+{
+	char letters;
+	int counter;
+
+	for (counter = 1; counter <= 10; counter++)
+	{
+		for (letters = 'a'; letters <= 'z'; letters++)
+		{
+			_putchar(letters); 
+		}
+	}
+	_putchar('\n');
+}
