ad4b9ba5453c57f01a83239611d8082d01af4592
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 23:39:35 +0300

    updated task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,24 +7,25 @@
  */
 void jack_bauer(void)
 {
-    int hour1, hour2;
-    int minute1, minute2;
-    for (hour1 = 0; hour1 < 3; hour1++)
-    {
-        for(hour2 = 0; hour2 < 4; hour2++)
-        {
-            for(minute1 = 0; minute1 < 6; minute1++)
-            {
-                for(minute2 = 0; minute2 < 10; minute2++)
-                {
-                    putchar(hour1 + '0');
-                    putchar(hour2 + '0');
-                    putchar(':');
-                    putchar(minute1 + '0');
-                    putchar(minute2 + '0');
-                    putchar('\n');
-                }
-            }
-        }
-    }
+	int hour1, hour2;
+	int minute1, minute2;
+	
+	for (hour1 = 0; hour1 < 3; hour1++)
+	{
+		for(hour2 = 0; hour2 < 4; hour2++)
+		{
+			for(minute1 = 0; minute1 < 6; minute1++)
+			{
+				for(minute2 = 0; minute2 < 10; minute2++)
+				{
+					_putchar(hour1 + '0');
+					_putchar(hour2 + '0');
+					_putchar(':');
+					_putchar(minute1 + '0');
+					_putchar(minute2 + '0');
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
