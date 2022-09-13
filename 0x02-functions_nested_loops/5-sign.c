23267a1444414e41643a109e75a567d4a7ad3c03
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 21:13:35 +0300

    updated task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,19 +10,23 @@
  */
 int print_sign(int n)
 {
+	int returnValue;
+
 	if (n > 0)
 	{
 		_putchar('+');
-		return (1);
+		returnValue = 1;
 	}
 	else if (n == 0)
 	{
 		_putchar(48);
-		return (0);
+		returnValue = 0;
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
+		returnValue = -1;
 	}
+
+	return (returnValue);
 }
