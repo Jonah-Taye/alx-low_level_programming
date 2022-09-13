cbfa99a06ce624820728fbb60b893c0977d04039
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 23:35:48 +0300

    task 8 and updated main.h

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * jack_bauer - Prints the time in 24 hr format
+ *
+ * Return: Void
+ */
+void jack_bauer(void)
+{
+    int hour1, hour2;
+    int minute1, minute2;
+    for (hour1 = 0; hour1 < 3; hour1++)
+    {
+        for(hour2 = 0; hour2 < 4; hour2++)
+        {
+            for(minute1 = 0; minute1 < 6; minute1++)
+            {
+                for(minute2 = 0; minute2 < 10; minute2++)
+                {
+                    putchar(hour1 + '0');
+                    putchar(hour2 + '0');
+                    putchar(':');
+                    putchar(minute1 + '0');
+                    putchar(minute2 + '0');
+                    putchar('\n');
+                }
+            }
+        }
+    }
+}
