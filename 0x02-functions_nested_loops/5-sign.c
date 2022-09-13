37e618311e180399ece932ae0d921c2b9f1afe09
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 20:36:17 +0300

    task 5 and updated main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - Prints the sign of a number and returns 1, 0 or -1
+ * @n: The integer that is going to be checked
+ *
+ * Return: 1 if the integer is greater than 0
+ * Return: 0 if the integer is zero
+ * Return: -1 if the integer is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
