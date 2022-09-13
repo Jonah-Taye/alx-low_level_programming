558f6175990007b98bf5add6ad92b532c25ad2f5
Yonas <jonahtaye.yg@gmail.com>
2022-09-13 22:17:10 +0300

    task 6 & 7 and updated main.h

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Finds the last digit of a number
+ * @n: The number used for finding the last digit
+ *
+ * Return: The last digit of the integer
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
