1b9a22235a8a0f0276c83156a51148d4c1478daa
Yonas <jonahtaye.yg@gmail.com>
2022-09-14 04:55:31 +0300

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Adds two integers and returns the sum
+ * @n: Accepts the number
+ *
+ * Return: Void
+ */
+void print_to_98(int n)
+{
+    	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i > 97; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+	{
+		for (i = n; i < 99; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+
+	}
+	printf("\n");
+}
