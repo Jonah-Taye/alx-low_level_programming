6f464b3d1f33f99d786683c055c8ec9b058aadd2
Jonah <jonahtaye.yg@gmail.com>
2022-09-15 22:07:33 +0300

    eighth update for task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,12 +8,12 @@
  * @year: year
  * Return: void
  */
-   
+
 void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0) && !(year % 100 == 0))
 	{
-		if (month >= 2 && day > 60)
+		if (month > 2)
 		{
 			day++;
 		}
@@ -23,7 +23,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else if (year % 400 == 0)
 	{
-		if (month >= 2 && day > 60)
+		if (month > 2)
 		{
 			day++;
 		}
