974b4778df55423c64dda9bc99f3808599e9e8c6
Jonah <jonahtaye.yg@gmail.com>
2022-09-20 05:28:58 +0300

    second update for task 9 and updated task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,19 +12,17 @@
 
 void print_array(int *a, int n)
 {
-	int i = 0;
+	int i;
 
-	while (*a != '\0')
+	for (i = 0; i < n; i++)
 	{
-		while (i < n)
+		if (i != n - 1)
 		{
-			printf("%d", a[0]);
-			if (i != n - 1)
-			{
-				printf(", ");
-			}
-			i++;
-			a++;
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
 		}
 	}
 	printf("\n");
