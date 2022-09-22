b7b1f48410b238ada90cf63d4c98fcd1f3c09b38
Jonah <jonahtaye.yg@gmail.com>
2022-09-22 20:01:14 +0300

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include <string.h>
+
+/**
+ * leet - Changes the string into a leet code
+ * @c: Accepts the string
+ *
+ * Return: Converted string
+ */
+
+char *leet(char *c)
+{
+	char checker[] = "a4A4e3E3o0O0t7T7l1L1";
+	int i;
+	int j;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		for (j = 0; checker[j] != '\0'; j++)
+		{
+			if (c[i] == checker[j])
+			{
+				c[i] = checker[j + 1];
+				break;
+			}
+		}
+	}
+	return (c);
+}
