ea80f4584c8dd79493275a1621b77e3e40e38245
Jonah <jonahtaye.yg@gmail.com>
2022-09-22 17:24:25 +0300

    updated task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,16 +9,15 @@
 
 char *string_toupper(char *c)
 {
-    int i = 0;
-    int len = strlen(c);
+	int i = 0;
+	int len = strlen(c);
 
+	while (i < len && *c != '\0')
+	{
+		if (c[i] >= 'a' && c[i] <= 'z')
+			c[i] = c[i] - 32;
+		i++;
+	}
 
-    while (i < len && *c != '\0')
-    {
-        if (c[i] >= 'a' && c[i] <= 'z')
-            c[i] = c[i] - 32;
-        i++;
-    }
-
-    return (c);
+	return (c);
 }
