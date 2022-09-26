d61597b49bdce392d87f0788368cb2a4454c8e25
Jonah <jonahtaye.yg@gmail.com>
2022-09-27 00:57:47 +0300

    second udpate for task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -14,9 +14,9 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int count = 0;
 	int i, j;
 
-	for (i = 0; i < strlen(s); i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < strlen(accept); j++)
+		for (j = 0; s[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
